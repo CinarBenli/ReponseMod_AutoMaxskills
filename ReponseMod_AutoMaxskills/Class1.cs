36eec503f71e2c35ab608fbd24178d25bad3db95
Reponse Mod <cinarbenliiphone@hotmail.com>
2022-05-08 01:59:32 +0300

    Add Plugins

---
+++ ReponseMod_AutoMaxskills/Class1.cs
@@ -0,0 +1,42 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ReponseMod_AutoMaxskills
+{
+    public class Class1 : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            base.Load();    
+            U.Events.OnPlayerConnected += j;
+            UnturnedPlayerEvents.OnPlayerRevive += r;
+        }
+
+        private void r(UnturnedPlayer player, Vector3 position, byte angle)
+        {
+            if (Configuration.Instance.WarningMessage)
+            {
+                ChatManager.serverSendMessage(Configuration.Instance.WarningMessageText, Color.white, null, player.SteamPlayer(), EChatMode.SAY, Configuration.Instance.WarningLogo, true);
+            }
+            player.Player.skills.ServerUnlockAllSkills();
+        }
+
+        private void j(UnturnedPlayer player)
+        {
+            if (Configuration.Instance.WarningMessage)
+            {
+                ChatManager.serverSendMessage(Configuration.Instance.WarningMessageText, Color.white, null, player.SteamPlayer(), EChatMode.SAY, Configuration.Instance.WarningLogo, true);
+            }
+            player.Player.skills.ServerUnlockAllSkills();
+        }
+    }
+}
---
+++ ReponseMod_AutoMaxskills/Config.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.API;
+
+namespace ReponseMod_AutoMaxskills
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public bool WarningMessage;
+        public string WarningMessageText;
+        public string WarningLogo;
+
+        public void LoadDefaults()
+        {
+            WarningMessage = true;
+            WarningMessageText = "Your <color=orange>Skills</color> Have Been <color=green>Successfully</color> Completed!";
+            WarningLogo = "https://media.discordapp.net/attachments/959142220366237796/962008357990977626/122.png";
+        }
+    }
+}
